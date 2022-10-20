7ec7051c6a1e4bbc474613a4a5476e68a6ad9ef2
Wesley Beck <74019873+wizbeck21@users.noreply.github.com>
2022-10-19 21:31:05 -0600

    Add Item.js and stub Item class

---
+++ src/classes/Item.js
@@ -0,0 +1,11 @@
+class Item {
+  static all = [];
+
+  constructor(id, name, price, item_type, toppings) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.item_type = item_type;
+    this.toppings = toppings;
+  }
+}
